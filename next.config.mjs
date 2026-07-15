08519c3a45f50d546d1cfb932ec604560ab6e70d
unknown <pranavr1415@gmail.com>
2026-07-15 11:23:11 +0530

    Learning routing in next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   reactCompiler: true,
   experimental: {
     turbopackFileSystemCacheForDev: true,
-  reactCompiler: true,
   }
 };
 
