002862c5d0c63763cc19d049ea99d797647927b4
unknown <pranavr1415@gmail.com>
2026-07-17 20:20:46 +0530

    Bmi calculator formula has been updated + logic + ui

--- src/app/bmi/calculator/page.jsx
+++ src/app/bmi/calculator/page.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './calculator.module.css'
 
 const Calculator = () => {
-  return (
+
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [bmi , setBmi] = useState(0); 
+
+    const handleCalc = () => {
+        setBmi((weight)/(height**2))
+    }
+ 
+ 
+return (
     <div className={styles.calculator}>
         <h1>BMI Calculator</h1>
-        <form>
+        <form onSubmit={handleCalc}>
             <label htmlFor="weight">Weight (kg):</label>
-            <input type="number" id="weight" name="weight" required />
+            <input type="number" id="weight" name="weight" value={weight} onChange={(e) => {setWeight(e.currentTarget.value)}} required />
             <label htmlFor="height">Height (cm):</label>
-            <input type="number" id="height" name="height" required />
+            <input type="number" id="height" name="height" value={height} onChange={(e) => {setHeight(e.currentTarget.value)}} required />
             <button type="submit">Calculate BMI</button>
         </form>
         <div className={styles.result}>
-            <p>Your BMI will be displayed here.</p>
+            <p>Your BMI is {bmi}</p>
         </div>
     </div>
   )
