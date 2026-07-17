d43811d463ee2863e5e0860f437138da384b1ceb
unknown <pranavr1415@gmail.com>
2026-07-17 16:08:05 +0530

    basic stuf

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,14 @@
+import ExploreBtn from "../components/ExploreBtn";
+
 const page = () => {
   return (
       <section>
           <h1 className="text-center">Hub for all Gym Twins <br /> Stay fit!</h1>
           <p className="text-center mt-5">Welcome to the hub for all gym twins where you can be free.</p>
+      
+          <ExploreBtn />
+
+
       </section>
   )
 }
