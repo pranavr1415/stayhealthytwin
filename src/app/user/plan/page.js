0a5bfb809743f90c254833d0ea5dae4684ab7718
unknown <pranavr1415@gmail.com>
2026-07-18 04:42:13 +0530

    plan page starting

--- src/app/user/plan/page.js
+++ src/app/user/plan/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function Plan() {
+export default function Plan({ plan }) {
+    
   return (
     <div>Plan</div>
   )
---
+++ src/components/PlanView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  PlanView = ({ plan }) => {
+  return (
+    <div>PlanView</div>
+  )
+}
+
+export default PlanView
