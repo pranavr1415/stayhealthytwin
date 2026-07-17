5383ac4ce6b72c57bd4e2e99c8de47f5d578fe4b
unknown <pranavr1415@gmail.com>
2026-07-17 13:21:08 +0530

    homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from '../components/LightRays';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -23,7 +24,26 @@ export default function RootLayout({ children }) {
       className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+      <div className= "absolute inset-0 top-0 z--1 min-h-screen ">
+        <LightRays
+          raysOrigin="top-center-offset"
+          raysColor="#5dfeca"
+          raysSpeed={0.5}
+          lightSpread={0.9}
+          rayLength={1.4}
+          followMouse={true}
+          mouseInfluence={0.02}
+          noiseAmount={0}
+          distortion={0.01}
+          className="custom-rays"
+          pulsating={false}
+          fadeDistance={1}
+          saturation={1}
+      />
+      </div>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,9 @@
 const page = () => {
   return (
-      <h1>Welcome to Next.js 16</h1>
+      <section>
+          <h1 className="text-center">Hub for all Gym Twins <br /> Stay fit!</h1>
+          <p className="text-center mt-5">Welcome to the hub for all gym twins where you can be free.</p>
+      </section>
   )
 }
 export default page;
